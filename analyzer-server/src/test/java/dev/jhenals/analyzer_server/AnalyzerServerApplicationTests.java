dc6a968834896b4370d1b447b3dbdf1834fbe476
Jhenals <suboljhenalyn3@gmail.com>
2025-06-04 18:20:56 +0200

    fix: change return type of analyze_code method

--- analyzer-server/src/test/java/dev/jhenals/analyzer_server/AnalyzerServerApplicationTests.java
+++ analyzer-server/src/test/java/dev/jhenals/analyzer_server/AnalyzerServerApplicationTests.java
@@ -1,13 +1,19 @@
 package dev.jhenals.analyzer_server;
 
+import dev.jhenals.analyzer_server.models.Commit;
+import dev.jhenals.analyzer_server.models.PRInput;
+import dev.jhenals.analyzer_server.models.StaticAnalysisResult;
+import dev.jhenals.analyzer_server.service.StaticAnalysisService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class AnalyzerServerApplicationTests {
-
-//	@Test
-//	void contextLoads() {
-//	}
 
 }
