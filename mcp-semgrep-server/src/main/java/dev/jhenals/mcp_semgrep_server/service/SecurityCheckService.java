f9f0977838ddb658371ea6aadeda5e8f85c92374
Jhenals <suboljhenalyn3@gmail.com>
2025-06-11 13:28:22 +0200

    refactor: rename variables for clarity, remove unused classes

--- mcp-semgrep-server/src/main/java/dev/jhenals/mcp_semgrep_server/service/SecurityCheckService.java
+++ mcp-semgrep-server/src/main/java/dev/jhenals/mcp_semgrep_server/service/SecurityCheckService.java
@@ -3,14 +3,13 @@ package dev.jhenals.mcp_semgrep_server.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.jhenals.mcp_semgrep_server.models.CodeFile;
-import dev.jhenals.mcp_semgrep_server.models.SemgrepSecurityCheckResult;
+import dev.jhenals.mcp_semgrep_server.models.SecurityCheckResult;
 import dev.jhenals.mcp_semgrep_server.models.SemgrepToolResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 import static dev.jhenals.mcp_semgrep_server.utils.SemgrepUtils.*;
@@ -26,24 +25,23 @@ public class SecurityCheckService {
 
         try {
             @SuppressWarnings("unchecked")
-            List<Map<String, String>> codeFilesData = (List<Map<String, String>>) input.get("code_files");
-
             Map<String, String> codeFileMap = (Map<String, String>) input.get("code_file");
             CodeFile codeFile = new CodeFile(codeFileMap.get("filename"), codeFileMap.get("content"));
 
             temporaryFileAbsolutePath = createTemporaryFile(codeFile).getAbsolutePath();
 
-            //semgrep --config "p/security-code-scan"
-            ArrayList<String> commands = new ArrayList<>(Arrays.asList("semgrep",
+            //semgrep --config p/security-code-scan
+            ArrayList<String> commands = new ArrayList<>(Arrays.asList(
+                    "semgrep",
                     "--config", "p/security-code-scan",
                     "--json",
                     "--quiet",
                     "--no-git-ignore"));
             commands.add(temporaryFileAbsolutePath);
             JsonNode output= runSemgrepService(commands, temporaryFileAbsolutePath);
-            log.info(output.toPrettyString());
+            log.info("Json output: {}", output.toPrettyString());
 
-            SemgrepSecurityCheckResult securityCheckResult = null;
+            SecurityCheckResult securityCheckResult;
 
             if (!output.get("results").isEmpty()) {
                 String message = output.get("results") + " security issues found in the code.\n\n" +
@@ -52,9 +50,9 @@ public class SecurityCheckService {
                         objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString("results") +
                         "\n</security-issues>";
 
-                securityCheckResult = new SemgrepSecurityCheckResult(message);
+                securityCheckResult = new SecurityCheckResult(message);
             } else {
-                securityCheckResult = new SemgrepSecurityCheckResult("No security issues found in the code!");
+                securityCheckResult = new SecurityCheckResult("No security issues found in the code!");
             }
             return SemgrepToolResult.securityCheckSuccess(securityCheckResult);
         } catch (Exception e) {
