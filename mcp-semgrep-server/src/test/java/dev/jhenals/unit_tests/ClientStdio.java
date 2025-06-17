0c48fef6b2b4a55ca2b663c97656b67f94168836
Jhenals <suboljhenalyn3@gmail.com>
2025-06-17 11:36:57 +0200

    feat: add tool testing (semgrep_scan) in ClientStdio class

--- mcp-semgrep-server/src/test/java/dev/jhenals/unit_tests/ClientStdio.java
+++ mcp-semgrep-server/src/test/java/dev/jhenals/unit_tests/ClientStdio.java
@@ -1,15 +1,15 @@
 package dev.jhenals.unit_tests;
 
 import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.McpSyncClient;
 import io.modelcontextprotocol.client.transport.ServerParameters;
 import io.modelcontextprotocol.client.transport.StdioClientTransport;
-import io.modelcontextprotocol.spec.McpClientTransport;
 import io.modelcontextprotocol.spec.McpSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.nio.file.Paths;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,7 +32,13 @@ public class ClientStdio {
 					"C:\\.My_Projects\\mcp\\AI-Code-Review-Assistant\\mcp-semgrep-server\\target\\mcp-semgrep-server-0.0.1-SNAPSHOT.jar")			.build();
 
 		var transport = new StdioClientTransport(stdioParams);
-        var client = McpClient.sync(transport).build();
+        McpSyncClient client = McpClient.sync(transport)
+                .requestTimeout(Duration.ofMinutes(10))
+                .capabilities(McpSchema.ClientCapabilities.builder()
+                        .roots(true)      // Enable roots capability
+                        .sampling()       // Enable sampling capability
+                        .build())
+                .build();
 
         var result = client.initialize();
         log.info("CLIENT initialized: {}", result);
@@ -41,6 +47,24 @@ public class ClientStdio {
 		McpSchema.ListToolsResult toolsList = client.listTools();
         log.info("AVAILABLE TOOLS = {}", toolsList);
 
+        log.info("-------TOOL TESTING-----------------------------------------");
+
+        log.info("Tool 1: Semgrep Scan----------------------------------------");
+        Map<String, Object> semgrepInput= new HashMap<>();
+        semgrepInput.put("config", "auto");
+        Map<String, String> codeFile= new HashMap<>();
+        codeFile.put("filename", "Example.java");
+        codeFile.put("content", "public class Example { public void doSomething() { System.out.println(\"Hello\"); } }");
+        semgrepInput.put("code_file", codeFile);
+
+        McpSchema.CallToolRequest request = new McpSchema.CallToolRequest("semgrep_scan", Map.of("input", semgrepInput));
+        log.info("Input to semgrepScan tool: {}", request.arguments());
+
+        McpSchema.CallToolResult semgrepScanResult = client.callTool(request);
+
+        log.info("Semgrep scan result: {}", semgrepScanResult);
+
+        client.closeGracefully();
 
     }
 
